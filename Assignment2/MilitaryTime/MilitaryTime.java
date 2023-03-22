eba75e1d2fce66918ab68db0651e3a8beb69851e
rzbrto <rzbrto@gmail.com>
2023-03-22 05:42:44 -0700

    Finished MilitaryTime

--- Assignment2/MilitaryTime/MilitaryTime.java
+++ Assignment2/MilitaryTime/MilitaryTime.java
@@ -9,10 +9,16 @@ public class MilitaryTime {
         int firstTime = kbd.nextInt();
         System.out.println("Please enter the second time: ");
         int secondTime = kbd.nextInt();
+        int firstTimeMinutes = ((firstTime / 100) * 60) + (firstTime % 100);
+        int secondTimeMinutes = ((secondTime / 100) * 60) + (secondTime % 100);
+        int differenceInMinutes = secondTimeMinutes - firstTimeMinutes;
 
+        if (differenceInMinutes < 0) {
+            differenceInMinutes = (24 * 60) + differenceInMinutes;
+        }
+        System.out.printf("The difference in the two times is %d hour(s) and %d minutes(s)", differenceInMinutes / 60, differenceInMinutes % 60);
 
     }
-
 }
 /*
   Please enter the first time: 0050
@@ -22,6 +28,7 @@ public class MilitaryTime {
   Please enter the first time: 0050
   Please enter the second time: 0040
   The difference in the two times is 23 hour(s) and 50 minutes(s)
+
   Please enter the first time: 2359
   Please enter the second time: 0001
   The difference in the two times is 0 hour(s) and 2 minutes(s)
