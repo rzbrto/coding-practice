665f2edea7dd86e4a5a6bcc34983cda87a6d2245
rzbrto <rzbrto@gmail.com>
2023-03-30 02:59:12 -0400

    Finished BankAccount Finished Telephone

--- Assignment8/BankAccount.java
+++ Assignment8/BankAccount.java
@@ -14,24 +14,24 @@ public class BankAccount {
         String c = "4321-0 betterpassword 96.74";
 
         String userCreds = String.join(" ", accountNumber, password);
-        if (userCreds.equals(a.substring(0,a.lastIndexOf(" ")))){
+        if (userCreds.equals(a.substring(0, a.lastIndexOf(" ")))) {
             return a.substring(a.lastIndexOf(" ") + 1);
         }
-        if (userCreds.equals(b.substring(0,b.lastIndexOf(" ")))){
+        if (userCreds.equals(b.substring(0, b.lastIndexOf(" ")))) {
             return b.substring(b.lastIndexOf(" ") + 1);
         }
-        if (userCreds.equals(c.substring(0,c.lastIndexOf(" ")))) {
+        if (userCreds.equals(c.substring(0, c.lastIndexOf(" ")))) {
             return c.substring(c.lastIndexOf(" ") + 1);
         }
         return "wrong";
     }
 
-    public static float getBalance(){
+    public static float getBalance() {
         Scanner kbd = new Scanner(System.in);
         String balance;
         int attempts = 1;
         do {
-            if(attempts > 3) {
+            if (attempts > 3) {
                 System.exit(0);
             }
             System.out.println("Account Number:");
@@ -39,32 +39,61 @@ public class BankAccount {
             System.out.println("Password:");
             String password = kbd.nextLine();
             balance = checkID(accountNumber, password);
-            attempts ++;
+            attempts++;
         } while (balance.equals("wrong"));
         return Float.parseFloat(balance);
     }
 
     public static void printBalance(float balance) {
+        System.out.println("\nYour Current Balance is $" + balance + "\n");
         //Your Current Balance is $520.36
     }
 
     private static void printOptions() {
-        //Choose From the Following Options:
-// 1. Display Balance
-// 2. Deposit
-// 3. Withdraw
-// 4. Log Out
+        System.out.println("""
+                Choose From the Following Options:
+                    1. Display Balance
+                    2. Deposit
+                    3. Withdraw
+                    4. Log Out
+                """);
+
     }
+
     private static void logout() {
+        System.out.println("\nLOGGED OUT");
+        System.exit(0);
         //LOGGED OUT
     }
 
-    private static void withdraw() {
+    private static float withdraw(float balance) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("\nAmount to Withdraw: $");
+        float withdrawalAmount = Float.parseFloat(kbd.nextLine());
+        if (withdrawalAmount < 0) {
+            System.out.println("\ninvalid amount\n");
+        } else if (withdrawalAmount <= balance) {
+            balance -= withdrawalAmount;
+            System.out.printf("\nYour New Balance is $%.2f" + "\n", balance);
+        } else {
+            System.out.println("\ninsufficient funds\n");
+        }
+        return balance;
         //Amount to Withdrawl: $ 20
-    //Your New Balance is 520.36
+        //Your New Balance is 520.36
     }
 
-    private static void deposit() {
+    private static float deposit(float balance) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("\nAmount to Deposit: $");
+        float depositAmount = Float.parseFloat(kbd.nextLine());
+        if (depositAmount >= 0) {
+            balance += depositAmount;
+            System.out.printf("\nYour New Balance is $%.2f" + "\n", balance);
+        } else {
+            System.out.println("\ninvalid amount\n");
+        }
+        return balance;
         //Amount to Deposit: $ 20
 //Your New Balance is $540.36
     }
@@ -75,25 +104,19 @@ public class BankAccount {
 
         float balance = getBalance();
 
-        System.out.println("Balance: $" + balance);
+        System.out.println("\nBalance: $" + balance + "\n");
         while (true) {
             printOptions();
             String option = kbd.nextLine();
             switch (option) {
-                case "1":
-                    printBalance(balance);
-                case "2":
-                    deposit();
-                case "3":
-                    withdraw();
-                case "4":
-                    logout();
-                default:
-                    System.out.println("invalid option");
+                case "1" -> printBalance(balance);
+                case "2" -> balance = deposit(balance);
+                case "3" -> balance = withdraw(balance);
+                case "4" -> logout();
+                default -> System.out.println("invalid option");
             }
         }
     }
-
 
 
 }
---
+++ Assignment8/Telephone.java
@@ -0,0 +1,51 @@
+package Assignment8;
+
+import Assignment6.SwitchCase;
+
+import java.util.Scanner;
+
+public class Telephone {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Please enter a 10-digit telephone number: ");
+        String phoneNumber = kbd.nextLine();
+        int[] freq = new int[10];
+
+        for (char c : phoneNumber.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                freq[c - '0'] += 1;
+            }
+        }
+
+        System.out.println("The following is the number of times each digit appears:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " - " + freq[i]);
+        }
+    }
+}
+////Please enter a 10-digit telephone number: 2528643866
+////
+////The following is the number of times each digit appears:
+//0 - 0
+//1 - 0
+//2 - 2
+//3 - 1
+//4 - 1
+//5 - 1
+//6 - 3
+//7 - 0
+//8 - 2
+//9 - 0
+//Please enter a 10-digit telephone number: (252)864-3866
+//
+//The following is the number of times each digit appears:
+//0 - 0
+//1 - 0
+//2 - 2
+//3 - 1
+//4 - 1
+//5 - 1
+//6 - 3
+//7 - 0
+//8 - 2
+//9 - 0
