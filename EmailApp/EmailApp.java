be1a49877fc77566cd9dc1723153ff9d7a8b57f5
rzbrto <rzbrto@gmail.com>
2023-04-11 00:48:56 -0400

    Updated EmailApp

--- EmailApp/EmailApp.java
+++ EmailApp/EmailApp.java
@@ -3,9 +3,6 @@ package EmailApp;
 public class EmailApp {
     public static void main(String[] args) {
         Email em1 = new Email("john", "smith");
-
-        em1.setAlternateEmail("fds232@gmail.com");
         System.out.println(em1.showInfo());
-
     }
 }
